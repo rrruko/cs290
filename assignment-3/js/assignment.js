bb3c426693f552641ad51c64702e139e942ba62f
ruko <rukokarasu@gmail.com>
2018-01-28 13:35:03 -0800

    Made editable paragraphs

--- assignment-3/js/assignment.js
+++ assignment-3/js/assignment.js
@@ -1,18 +1,60 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
   let nodes = document.getElementsByClassName("tile");
+
   for (let i=0; i < nodes.length; i++) {
     let closeButtons = nodes[i].getElementsByClassName("close");
     for (let j = 0; j < closeButtons.length; j++) {
       let node = nodes[i];
       closeButtons[j].addEventListener("click", function () {
-          console.log("hiding node");
           hideTile(node);
       });
     }
 
   }
+
+  let paragraphs = document.getElementsByClassName("editable");
+
+  for (let i = 0; i < paragraphs.length; i++) {
+    let p = paragraphs[i];
+    imbueEditableEvent(p);
+  }
+
+  let forms = document.getElementsByTagName("form");
+
+  for (let i = 0; i < forms.length; i++) {
+    let form = forms[i];
+    let button = form.querySelector("button");
+    console.log("got " + button);
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      let maybeTextArea = form.querySelector("textarea");
+      if (maybeTextArea) {
+        let para = toParagraph(maybeTextArea);
+        form.replaceChild(para, maybeTextArea);
+        console.log("done");
+      } else {
+        console.log("???");
+      }
+    });
+  }
 });
+
+function imbueEditableEvent(p) {
+    p.addEventListener("dblclick", function () {
+    let newEl = document.createElement("textarea");
+    newEl.innerHTML = p.innerHTML;
+    p.parentNode.replaceChild(newEl, p);
+  });
+}
+
+function toParagraph(textarea) {
+  let para = document.createElement("p");
+  para.className += "editable";
+  para.innerHTML = textarea.value;
+  imbueEditableEvent(para);
+  return para;
+}
 
 function hideTile(tile) {
   if (!tile.className.includes("hidden")) {
